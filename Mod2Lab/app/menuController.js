dc8ad39c5d31a38a7e4fd39384593268a2d1deef
Keflin Duarte <keflin.rosa@gft.com>
2018-03-12 10:30:00 -0300

    Changes

---
+++ Mod2Lab/app/menuController.js
@@ -0,0 +1,17 @@
+app.controller('menuController', [
+    '$scope',
+    function($scope){
+        $scope.model = {title: 'Our Menu'};
+        
+                                        
+        $scope.$watch('model.mainDish', function (newValue, oldValue) {
+            if (newValue === 'BBQ Chicken Pizza') {
+                alert('You have selected the BBQ Chicken Pizza!');
+            }
+        });
+
+        $scope.changeMainDish = function (name, price) {
+            $scope.model.mainDish = {name: name, price: price};
+       }
+    }
+]);
