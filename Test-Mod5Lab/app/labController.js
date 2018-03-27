66656c4fa24f1949c9abf041da588fc01cff9742
Keflin Duarte <keflin.rosa@gft.com>
2018-03-27 11:03:35 -0300

    Test-Mod5Lab finished

--- Test-Mod5Lab/app/labController.js
+++ Test-Mod5Lab/app/labController.js
@@ -3,17 +3,20 @@ app.controller('labController', [
     function ($scope, registration) {
         $scope.reset = reset;
         $scope.submit = submit;
+        $scope.token = null;
         
         reset();
 
         function submit(model) {
             registration.submit(model).$promise
                 .then(function (response) {
-                    alert('success');
+                    $scope.token = response.token;
+                    reset();
                 },
                 function (response) {
-                    alert('error');
+                    alert('Error!');
                 });
+                alert('Submitted\n' + JSON.stringify(model));
         }
 
         function reset() {
